4376bbf732d013d7dd9e56e297d53f3a47d52c65
Ivan Lopez <ivnlpz20@gmail.com>
2022-09-06 13:39:42 -0700

    first commit

---
+++ likesassignment/likes.js
@@ -0,0 +1,18 @@
+  toplikesSpan = document.getElementById("topLikes");
+  
+  function updateLikes(id) {
+  toplikesSpan.innerText++;
+  }
+
+  midlikesSpan = document.getElementById("midLikes");
+
+  function likesUpdate (id) {
+  midlikesSpan.innerText++;
+  }
+
+  botlikesSpan = document.getElementById("botLikes");
+
+  function botLikesUpdate (id) {
+  botlikesSpan.innerText++;
+  }
+
