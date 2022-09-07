4d873dbee42fa905348ad5fe634e51396b6fa087
Ivan Lopez <ivnlpz20@gmail.com>
2022-09-07 10:38:33 -0700

    javascript practice day two

---
+++ buttonclickerassignment/buttonclicker.js
@@ -0,0 +1,12 @@
+function logout(element) {
+  element.innerText="logout"
+}
+
+function eraseText(element) {
+  element.remove();
+}
+
+function showAlert(element) {
+  alert("Ninja was liked");
+}
+
